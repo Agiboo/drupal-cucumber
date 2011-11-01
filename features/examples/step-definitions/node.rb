7cfab59503def3901986bbb353a3ddb19c84d9b6
Tom Verhaeghe <info@heartdriven.com>
2011-11-01 18:35:54 +0100

    cucumber was running too fast - added sleep time

--- features/examples/step-definitions/node.rb
+++ features/examples/step-definitions/node.rb
@@ -6,19 +6,26 @@ When /^I create a new page$/ do
 end
 
 Then /^that new node is created$/ do
-  sleep 5
+  #sleep 5
   page.should have_content "A fresh page"
 end
 
 When /^I create a new page that's promoted to the front page$/ do
-  visit 'admin/content'
-  click_link 'Add content'
-  click_link 'Basic page'
-  fill_in 'edit-title', :with => "Fresh on the front page"
-  fill_in 'edit-body-und-0-value', :with => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque iaculis orci porttitor lectus laoreet tempus. Ut sed lorem quis elit tempus tempus. Cras at tortor turpis, vitae ultrices risus. Cras ornare ultricies purus, quis fringilla nibh imperdiet id. Duis a ante non sem pretium pretium."
-  drupal_click_vertical_tab 'Publishing options'
-  check "edit-promote"
-  click_button 'edit-submit'
+  visit ''
+  click_link 'Content'
+  within_drupal_overlay do 
+    click_link 'Add content'
+  end
+  within_drupal_overlay do
+    click_link 'Basic page'
+  end
+  within_drupal_overlay do
+    fill_in 'edit-title', :with => "Fresh on the front page"
+    fill_in 'edit-body-und-0-value', :with => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque iaculis orci porttitor lectus laoreet tempus. Ut sed lorem quis elit tempus tempus. Cras at tortor turpis, vitae ultrices risus. Cras ornare ultricies purus, quis fringilla nibh imperdiet id. Duis a ante non sem pretium pretium."
+    drupal_click_vertical_tab 'Publishing options'
+    check "edit-promote"
+    click_button 'edit-submit'
+  end
 end
 
 Then /^I can see that new page on the front page$/ do
--- shared/methods/overlay.rb
+++ shared/methods/overlay.rb
@@ -1,4 +1,6 @@
 def within_drupal_overlay &block
+  # Adding an id to the iframe with class "overlay-active" because drupal doesn't give id's to those iframes
+  # Drupal tobbles between two iframes.  I think this is because of performance/responsiveness issues
   page.execute_script("jQuery('.overlay-active').attr('id','overlay-active');")
   i=0
   until overlay_visible || i > 10
@@ -9,6 +11,8 @@ def within_drupal_overlay &block
   within_frame 'overlay-active' do
     sleep 1
     yield
+    # Removing the id of the iframe with id "overlay-active" because with the next call, we'll need this
+    page.execute_script("jQuery('#overlay-active').attr('id','');")
   end
 end
 
